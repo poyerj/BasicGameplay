088f9ba34fa9ac74f822efb70ac3f728b4b682eb
poyerj <poyerj@roverkids.org>
2024-05-01 13:52:43 -0400

    2.2 complete

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch projectiles from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         if (transform.position.x < -xRange){
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
