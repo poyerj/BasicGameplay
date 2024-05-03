a4f16c7697dc292694adf5037e6fd0041ff3aa2b
poyerj <poyerj@roverkids.org>
2024-05-03 13:44:37 -0400

    2.3

--- BasicGameplay/Assets/Scripts/DestoryOutOfBounds.cs
+++ BasicGameplay/Assets/Scripts/DestoryOutOfBounds.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestoryOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
-    private float lowerBound = 10;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
